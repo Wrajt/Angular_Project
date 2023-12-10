0458c1b7f321c531dba0477df8a000aad544f4bd
Wrajt <kgrabarek59@gmail.com>
2023-12-10 12:31:35 +0100

    Property binding vs String Interpolation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server>  </app-server> <app-server>  </app-server>',
+  templateUrl: './servers.component.html',
   styleUrl: './servers.component.css'
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit{
+  allowNewServer = false;
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+  serverID: number = 10;
+  serverStatus: string = 'offline';
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  ngOnInit(): void {
+  }
 }
