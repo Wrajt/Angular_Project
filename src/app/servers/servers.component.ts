5bbafe46a186504926140bd8816234a65c14f178
Wrajt <kgrabarek59@gmail.com>
2024-01-17 16:05:22 +0100

    Directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit{
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName ='Testserver'
+  serverCreated = false;
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -25,6 +26,7 @@ export class ServersComponent implements OnInit{
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
   onUpdateServerName(event: any) {
